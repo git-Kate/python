6f94c9183cdc3e965ad14f7a339c63f1a523204a
your git-Kate <your_646642124@qq.com>
2021-09-05 22:30:52 +0800

    2021年9月5日22:30:49

--- exercise/Lesson3.py
+++ exercise/Lesson3.py
@@ -106,15 +106,86 @@
 #第三次循环取4，count统计次数等于一，不满足>1的条件，所以不执行print(list.index(i))
 #第三次循环取3，count统计次数大于一，满足>1的条件，所以执行print(list.index(i))输出3的下标1
 #冒泡排序
-list=[1,2,3,4,5,6,7,8]
-l=len(list)
-for y in range(l-1):
-    print(f"{l}-{1}")
-    for i in range(l-1-y):
-        print(f"{l}-{1}-{y}")
-        if list[i]<list[i+1]:
-            print(list)
-            list[i],list[i+1]=list[i+1],list[i]
-print(list)
+# list=[1,2,3,4,5,6,7,8]
+# l=len(list)
+# for y in range(l-1):
+#     print(f"{l}-{1}")
+#     for i in range(l-1-y):
+#         print(f"{l}-{1}-{y}")
+#         if list[i]<list[i+1]:
+#             print(list)
+#             list[i],list[i+1]=list[i+1],list[i]
+# print(list)
 
 #测试
+# if 'a' in 'abc':
+#     print('包含a')
+# list=[1,2,3,4,5,6]
+# if 1 in list:
+#     print('list包含1')
+# dict ={'name':'xiaowemg','age':'25'}
+# if 'name' in dict:
+#     print(f"{dict['name']}")
+
+# list = ['aaa', 'bbb', 'ccc']
+# if 'aaa' in list:  # 不包含，为假，所以不执行print('包含')
+#     print('为真')
+# list=[1,2,3,4,5,6]
+# for i in list:
+#     print(i)
+# dict ={'name':'xiaoweng','age':'25'}
+# for key in dict:
+#     print(f"{key}:{dict[key]}")
+# sum=0
+# for i in range(1,5):
+#     sum=sum+1
+#     print(sum)
+# Python index()
+# 方法检测字符串中是否包含子字符串 str ，如果指定 beg（开始） 和 end（结束） 范围，
+# 则检查是否包含在指定范围内，该方法与 python find()方法一样，只不过如果str不在 string中会报一个异常。
+# Python count() 方法用于统计字符串里某个字符或子字符串出现的次数。可选参数为在字符串搜索的开始与结束位置。
+# list=[1,3,4,3,3,5,6,7]#统计列表中重复的数，并打印出下标的方法
+# for i in list:
+# # print(list.count(3))
+#     if list.count(i)>1:#统计字符串出现次数
+#         print(list.index(i))
+
+# Python len() 方法返回对象（字符、列表、元组等）长度或项目个数。
+# #冒泡排序
+# list=[1,2,3,4,5,6,7,8]
+# l=len(list)
+# for i in range(l - 1):
+#     for y in range(l-1-i):
+#             if list[y]<list[y+1]:
+#                 list[y],list[y+1]=list[y+1],list[y]
+# print(list)
+# 异常处理
+# 捕捉异常可以使用try/except语句。
+# try/except语句用来检测try语句块中的错误，从而让except语句捕获异常信息并处理。
+# 如果你不想在异常发生时结束你的程序，只需在try里捕获它。
+# 语法：
+# 以下为简单的try....except...else的语法：
+# try:
+# <语句>        #运行别的代码
+# except <名字>：
+# <语句>        #如果在try部份引发了'name'异常
+# except <名字>，<数据>:
+# <语句>        #如果引发了'name'异常，获得附加的数据
+# else:
+# <语句>        #如果没有异常发生
+# try:
+#     fh = open("testfile", "w")
+#     fh.write("这是一个测试文件，用于测试异常!!")
+# except IOError:
+#     print("Error: 没有找到文件或读取文件失败")
+# else:
+#     print("内容写入文件成功")
+#     fh.close()
+# try:
+#     print("2"+2)
+# except TypeError:#如果发生TypeError异常，则执行下面的语句
+#     print('数字与字符串不能相加')
+# try:
+#     print("2"+2)
+# except Exception as e:#统计所有的异常
+#     print('统计所有的异常',e)
