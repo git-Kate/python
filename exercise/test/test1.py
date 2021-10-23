5bc2f45e5b17903c45c88e4a815c3a853b84ec25
your git-Kate <your_646642124@qq.com>
2021-10-23 22:06:55 +0800

    2021年10月23日22:06:50

---
+++ exercise/test/test1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time     :2021/10/23 下午 10:03
+# @Author   :yutq.test
+# @Email    :646642124@qq.com
+# @File     :test1.py
+# import os
+# result = os.getcwd()
+# print(result)
